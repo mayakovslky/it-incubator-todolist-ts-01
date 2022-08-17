52bc47cbc6c411a363df2a2165dae9015ddd8c5c
mayakovsky <mayakovsky03@gmail.com>
2022-08-17 12:58:27 +0300

    Add post, test and user component

---
+++ src/Post/Post.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Post = () => {
+    return (
+        <div>
+            <h2>
+                User
+            </h2>
+        </div>
+    )
+}
---
+++ src/User/User.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const User = () => {
+    return (
+        <div>
+            <h2>
+                User
+            </h2>
+        </div>
+    )
+}
